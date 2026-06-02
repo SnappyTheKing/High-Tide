c0c09708d299d4dc33798dc50323961fe7d85f03
SnappyTheKing <Snappy12234@gmail.com>
2026-06-01 20:50:28 -0400

    Balancing

--- high-tide/Scenes/item.gd
+++ high-tide/Scenes/item.gd
@@ -23,5 +23,5 @@ func _on_button_mouse_entered() -> void:
 	button.tooltip_text = "Sell for $" + str(snapped(calculate_value(value, origin, Globals.current_port_position), 0.01))
 
 func calculate_value(base: float, item_pos: Vector2, port_pos: Vector2) -> float:
-	var mult = sqrt(item_pos.distance_to(port_pos) + 1) / 200
+	var mult = sqrt(item_pos.distance_to(port_pos) + 1) / 150
 	return (base * mult) + (base * 0.5)
